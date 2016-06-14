a8f7038666a956bec0c103348c04978a61546065
Matt Yost <matt.yost2@gmail.com>
2016-06-14 16:34:22 -0500

    Added quotesArray and getRandomQuote function
    getRandomQuote function is not complete, but I think I have the right
    idea

--- js/script.js
+++ js/script.js
@@ -2,3 +2,96 @@
 // when user clicks anywhere on the page, the "makeQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+var quotesArray = [
+  {
+    quote: "I hated every minute of training, but I said 'Don't quit. Suffer now and live the rest of your life as a champion.",
+    source: "Muhammad Ali",
+    citation: "",
+    year: "",
+    tags: "inspirational"
+  },
+  {
+    quote: "The best and most beautiful things in the world cannot be seen or even touched. They must be felt with the heart.",
+    source: "Helen Keller",
+    citation: "",
+    year: "",
+    tags: "inspirational"
+  },
+  {
+    quote: "Strive to be whatever you want to be. Fate might be a lie, but if it isn't, then worrying abut it won't change a thing.",
+    source: "Alyn Shir",
+    citation: "Kingdoms of Amalur: Reckoning",
+    year: "",
+    tags: "fate"
+  },
+  {
+    quote: "What you do today can improve all your tomorrows.",
+    source: "Ralph Marston",
+    citation: "",
+    year: "",
+    tags: "motivation"
+  },
+  {
+    quote: "It is the mark of an educated mind to be able to entertain a thought without accepting it.",
+    source: "Aristotle",
+    citation: "",
+    year: "",
+    tags: "education"
+  },
+  {
+    quote: "The whole purpose of education is to turn mirrors into windows.",
+    source: "Sydney J. Harris",
+    citation: "",
+    year: "",
+    tags: "education"
+  },
+  {
+    quote: "Education is the most powerful weapon which you can use to change the world.",
+    source: "Nelson Mandela",
+    citation: "",
+    year: "",
+    tags: "education"
+  },
+  {
+    quote: "Life is really simple, but we insist on making it complicated",
+    source: "Confucius",
+    citation: "",
+    year: "",
+    tags: "life"
+  },
+  {
+    quote: "Life is not a problem to be solved, but a reality to be experimented.",
+    source: "Soren Kierkegaard",
+    citation: "",
+    year: "",
+    tags: "life"
+  },
+  {
+    quote: "You have enemies? Good. That means you've stood up for something, sometime in your life.",
+    source: "Winston Churchill",
+    citation: "",
+    year: "",
+    tags: "life"
+  }
+];
+
+// Create a function to get a random number? Might be more readable to just store it in a variable inside the function. UNLESS I need it globally.
+
+//I was trying to make it harder than it should have been. [i] is what needed to be random.
+//But now how am I going to make sure that it doesn't repeat?
+
+
+// Also this for loop in probably useless?. Does anyone know what they're doing?
+// for(var i=0; i < quotesArray.length; i++) {
+//   return quotesArray[i].quote;
+// }
+
+var getRandomQuote = function() {
+  console.log(quotesArray[Math.floor(Math.random() * quotesArray.length)].quote);
+}
+
+var printQuote = function() {
+  console.log('Print Quote');
+}
+
+getRandomQuote()
